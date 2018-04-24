4e28676382bed406133ef6ab22fcabc242568e7f
Jagestah <ickshnab@gmail.com>
2018-04-24 01:19:37 -0600

    userLogin updated to saving x-auth-token

--- getDevices.js
+++ getDevices.js
@@ -1,24 +1,31 @@
 var curl = require('curlrequest');
 var fs = require('fs');
-var token = "";
-var data = "";
-fs.readFile('/home/jj/Electron/unms-control-page/token.txt', 'utf8', data);
-	return data;
-console.log(data);
-
-
-
+var token = fs.readFileSync('/home/jj/Electron/unms-control-page/token', 'utf8');
 var options = {
 	url: 'https://35.193.18.18/v2.1/devices',
 	method: 'GET',
 	headers: {"accept": "application/json","content-type": "application/json","x-auth-token": token},
-	include: true,
+	include: false,
 	insecure: true
 }
 
-console.log(options);
+//console.log(options);
 
 curl.request(options, function(err, parts) {
-	console.log(err)
-	console.log(parts)
-})
+//	console.log(err)
+//	console.log(parts)
+	parts = JSON.stringify(parts)
+//	console.log(parts)
+	obj = JSON.parse(parts)
+//	console.log(obj)
+	console.log(obj.identification)
+});
+//var jsonarray = "";
+//JSONArray jsonarray = new JSONArray(parts);
+//for (int i = 0; i < jsonarray.length(); i++) {
+//    JSONObject jsonobject = jsonarray.getJSONObject(i);
+//    String name = jsonobject.getString("name");
+//    String url = jsonobject.getString("type");
+//}
+//console.log(name)
+//console.log(url)
--- userLogin.js
+++ userLogin.js
@@ -1,14 +1,28 @@
 var curl = require('curlrequest');
+var fs = require('fs');
+var data = '{"password": "'+'$umm1t20ie'+'","username": "ubnt","sessionTimeout": 3600000}'
 var options = {
 	url: 'https://35.193.18.18/v2.1/user/login',
 	method: 'POST',
 	headers: {"accept": "application/json","content-type": "application/json"},
 	data: '{"password": "$umm1t20ie","username": "ubnt","sessionTimeout": 3600000}',
-	include: true,
+	include: false,
 	insecure: true
 }
-
+//Parses info and saves token
 curl.request(options, function(err, parts) {
-	console.log(parts)
-	console.log(err)
+	var parts = JSON.stringify(parts)
+	var parts = parts.split('\\r\\n', -1)
+//	console.log(parts)
+	var token = parts[6]
+//	console.log(parts[6])
+	var token = token.split(": ", -1)
+	var token = token[1]
+//	console.log(token)
+	fs.writeFile('/home/jj/Electron/unms-control-page/token', token, function(err) {
+    	if(err) {
+        	return console.log(err);
+    	}
+    	console.log("The token was saved!");
+	});
 })
